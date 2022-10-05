8eb5653ff144b43816a551c67f168da5d3196ed3
panda1 <blueblue0011@gmail.com>
2022-10-04 18:58:43 -0700

    it works, and full back up

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import dbhelpers as dh
 import json
 
@@ -14,6 +14,15 @@ def client_list_info():
     else:
         print('error, error, error ')
 
+@app.get('/api/loyal_clients')
+def points_greater_than():
+    min_point = request.args.get('min_point')
+    result = dh.run_statement('CALL client_point_greater(?)', [min_point])
+    if(type(result) == list):
+        result_json = json.dumps(result, default=str)
+        return result_json
+    else:
+        print('error, error, error ')
 
 
 app.run(debug=True)
